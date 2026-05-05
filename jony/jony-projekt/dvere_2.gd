a216be739af1de39e11338532f8604501158281e
Jakub-Doucek <jakubdoucek@gmail.com>
2026-05-05 17:10:58 +0200

    sklep

---
+++ jony/jony-projekt/dvere_2.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		$AnimationPlayer.play("open")
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		$AnimationPlayer.play_backwards("open")
